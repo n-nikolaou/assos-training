29fdc19786e14bc9b651067d4bebe9b241123905
Nikos <nnikolaoug@csd.auth.gr>
2022-12-20 03:26:27 +0200

    incomplete commit

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {MongoClient} from "mongodb";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,28 @@ export class GameService {
   constructor(private http: HttpClient) { }
 
   getGames() {
-    console.log("!")
+// Connection URI
+    const uri =
+      "mongodb://localhost:27017";
+
+// Create a new MongoClient
+    const client = new MongoClient(uri);
+
+    async function run() {
+      try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+
+        // Establish and verify connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Connected successfully to server");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+    console.log("!");
     return this.http.get(this.baseUrl);
   }
 }
